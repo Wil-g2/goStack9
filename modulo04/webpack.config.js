08fd315a3cf7a619ccf1ccc385aa3e392a1f49ee
wil-g2 <wil-g2@hotmail.com>
2019-10-28 18:04:27 -0300

    chore: module 04, config babel and webpack

---
+++ modulo04/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path'); 
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'public'), 
+    filename: 'bundle.js'
+  }, 
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      //criar uma regra para toda vez que o babel encontrar um arquivo js ele deve transpilar
+      {
+        test: /\.js$/, 
+        exclude: /node_modules/, 
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader'},
+          { loader: 'css-loader'},
+        ],
+      },
+      {
+        test: /.*\.(gif|png|jpe?g)$/i, 
+        use: {
+          loader: 'file-loader'
+        }
+      }
+    ]
+  }
+};
