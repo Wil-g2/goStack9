b5ac88ee2270f7697a288ab3de6afca9904d7201
wil-g2 <wil-g2@hotmail.com>
2019-10-30 12:13:10 -0300

    feat: create PostList component.

---
+++ desafio-04/src/components/PostList/index.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react'
+import './styles.css';
+
+class PostList extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      posts: [
+        {
+          id: 1,
+          author: {
+            name: "Julio Alcantara",
+            avatar: "http://url-da-imagem.com/imagem.jpg"
+          },
+          date: "04 Jun 2019",
+          content: "Pessoal, alguém sabe se a Rocketseat está contratando?",
+          comments: [
+            {
+              id: 1,
+              author: {
+                name: "Diego Fernandes",
+                avatar: "http://url-da-imagem.com/imagem.jpg"
+              },
+              content: "Conteúdo do comentário"
+            }
+          ]
+        },
+        {
+          id: 2,
+          author: {
+            name: "Julio Alcantara",
+            avatar: "http://url-da-imagem.com/imagem.jpg"
+          },
+          date: "04 Jun 2019",
+          content: "Pessoal, alguém sabe se a Rocketseat está contratando?",
+          comments: [
+            {
+              id: 1,
+              author: {
+                name: "Diego Fernandes",
+                avatar: "http://url-da-imagem.com/imagem.jpg"
+              },
+              content: "Conteúdo do comentário"
+            }
+          ]
+        }
+      ]
+    }
+  }
+
+  render(){    
+    return(
+      <section>
+        <ul>
+          {this.state.posts.map(post => <li key={post.id}>{post.author.name}</li>)}            
+        </ul>  
+      </section>
+    );
+  }
+}
+export default PostList;
