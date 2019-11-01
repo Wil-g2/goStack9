3c9a02fb84de56a9c14e16b20a35b24b5aa61dae
wil-g2 <wil-g2@hotmail.com>
2019-11-01 14:49:43 -0300

    feat: create Post component.

--- desafio-04/src/components/Post/index.js
+++ desafio-04/src/components/Post/index.js
@@ -1,8 +1,25 @@
 import React from 'react'; 
+import './styles.css';
+import Comment from '../Comment';
 
 function Post ({ post }){
-    return(
-    
+  console.log(post);
+  return(    
+      <div className="content">
+      <ul className="post">
+        <li>
+          <div className="info">
+            <img src={post.author.avatar} alt="profile image"/>
+            <div>
+              <strong>{post.author.name}</strong>
+              <span>{post.date}</span>
+            </div>
+          </div>
+          <p>{post.content}</p>
+          <Comment comments={post.comments} />                  
+        </li>            
+      </ul>              
+    </div>
     )
 }
 
