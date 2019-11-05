f6f7576cca9ea6f1c80cb4688b0c1c0bc60aafa7
Willian Gaudencio de Rezende <wil-g2@hotmail.com>
2019-11-04 23:01:09 -0300

    chore: create style repository.

---
+++ modulo05/src/pages/Repository/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  max-width: 700px;
+  background: #fff;
+  border-radius: 4px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+  padding: 30px;
+  margin: 80px auto;
+  h1 {
+    display: flex;
+    flex-direction: row;
+
+    svg {
+      margin-right: 10px;
+    }
+  }
+`;
