02139a3bcdc0d024c621f409e35b8f56db363fd8
Willian Gaudencio de Rezende <wil-g2@hotmail.com>
2019-11-04 22:55:21 -0300

    chore: create functions in Repository.

--- modulo05/src/pages/Repository/index.js
+++ modulo05/src/pages/Repository/index.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Repository() {
-  return <h1>Repository</h1>;
+import { Link } from 'react-router-dom';
+import axios from '../../services/api';
+import { Container } from './styles';
+
+class Repository extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      repositories: '',
+    };
+  }
+
+  componentDidMount() {
+    this.getRepos();
+  }
+
+  getRepos = async () => {
+    const { repository } = this.props.match.params;
+    const res = await axios.get(`/repos/${decodeURIComponent(repository)}`);
+    const { data } = res;
+    console.log(data);
+    this.setState({ repositories: data });
+  };
+
+  render() {
+    const { repositories } = this.state;
+    return (
+      <Container>
+        <div>
+          <h1>Repositories</h1>
+          <strong>{repositories.id}</strong>
+          <Link to="/">Voltar</Link>
+        </div>
+      </Container>
+    );
+  }
 }
-
 export default Repository;
