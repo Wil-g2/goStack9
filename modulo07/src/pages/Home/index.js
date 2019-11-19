c7388e042566d810ae07b8dbcfa1301dc82f4091
wil-g2 <wil-g2@hotmail.com>
2019-11-19 18:32:25 -0300

    feat: create home layout.

--- modulo07/src/pages/Home/index.js
+++ modulo07/src/pages/Home/index.js
@@ -1,5 +1,100 @@
 import React from 'react';
+import { MdShoppingCart } from 'react-icons/md';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTP6WHxVINA1WLM4WBZj-UKJ-CNU6mTy0SoipoNy98WFBYHr-SH"
+          alt=""
+        />
+        <strong> Tênis Nike Air</strong>
+        <span> R$ 259,90 </span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart color="#fff" size={16} />
+          </div>
+          <span> ADICIONAR AO CARRINHO </span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTP6WHxVINA1WLM4WBZj-UKJ-CNU6mTy0SoipoNy98WFBYHr-SH"
+          alt=""
+        />
+        <strong> Tênis Nike Air</strong>
+        <span> R$ 259,90 </span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart color="#fff" size={16} />
+          </div>
+          <span> ADICIONAR AO CARRINHO </span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTP6WHxVINA1WLM4WBZj-UKJ-CNU6mTy0SoipoNy98WFBYHr-SH"
+          alt=""
+        />
+        <strong> Tênis Nike Air</strong>
+        <span> R$ 259,90 </span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart color="#fff" size={16} />
+          </div>
+          <span> ADICIONAR AO CARRINHO </span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTP6WHxVINA1WLM4WBZj-UKJ-CNU6mTy0SoipoNy98WFBYHr-SH"
+          alt=""
+        />
+        <strong> Tênis Nike Air</strong>
+        <span> R$ 259,90 </span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart color="#fff" size={16} />
+          </div>
+          <span> ADICIONAR AO CARRINHO </span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTP6WHxVINA1WLM4WBZj-UKJ-CNU6mTy0SoipoNy98WFBYHr-SH"
+          alt=""
+        />
+        <strong> Tênis Nike Air</strong>
+        <span> R$ 259,90 </span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart color="#fff" size={16} />
+          </div>
+          <span> ADICIONAR AO CARRINHO </span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTP6WHxVINA1WLM4WBZj-UKJ-CNU6mTy0SoipoNy98WFBYHr-SH"
+          alt=""
+        />
+        <strong> Tênis Nike Air</strong>
+        <span> R$ 259,90 </span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart color="#fff" size={16} />
+          </div>
+          <span> ADICIONAR AO CARRINHO </span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
