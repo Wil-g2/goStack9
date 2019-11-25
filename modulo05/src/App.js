5d4f9d0aca9f968bb086043854e31139bb2faf34
Willian Gaudencio de Rezende <wil-g2@hotmail.com>
2019-11-24 23:22:25 -0300

    feat: add ToastContainer.

--- modulo05/src/App.js
+++ modulo05/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-
+import { ToastContainer } from 'react-toastify';
 import Routes from './routes';
 import GlobalStyle from './styles/global';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <>
       <Routes />
       <GlobalStyle />
+      <ToastContainer draggable position="top-center" />
     </>
   );
 }
