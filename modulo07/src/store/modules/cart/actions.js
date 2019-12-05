32d28db31b957efbe9e2fa6b44fdab9f8905f2be
Willian Gaudencio de Rezende <wil-g2@hotmail.com>
2019-12-04 23:20:33 -0300

    feat: add remove method and update_amount method.

--- modulo07/src/store/modules/cart/actions.js
+++ modulo07/src/store/modules/cart/actions.js
@@ -1,8 +1,28 @@
-export function addToCart(product) {
+export function addToCartRequest(id) {
   return {
-    type: 'ADD_TO_CART',
+    type: '@cart/ADD_REQUEST',
+    id,
+  };
+}
+
+export function addToCartSuccess(product) {
+  return {
+    type: '@cart/ADD_SUCCESS',
     product,
   };
 }
 
-export function removeFromCart(id) {}
+export function removeFromCart(id) {
+  return {
+    type: '@cart/REMOVE',
+    id,
+  };
+}
+
+export function updateAmount(id, amount) {
+  return {
+    type: '@cart/UPDATE_AMOUNT',
+    id,
+    amount,
+  };
+}
